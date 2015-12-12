1f65364c2e7db2205ab7880617d87e15eb819eb6
Mikko Pohja <mjpohja@gmail.com>
2015-12-12 21:18:44 +0200

    Initial screenshot start

---
+++ Assets/Screencap.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Screencap : MonoBehaviour {
+
+    DateTime prevCapture;
+
+	// Use this for initialization
+	void Start () {
+        prevCapture = DateTime.Now;
+        print(Application.persistentDataPath);
+        SaveScreenshot();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (DateTime.Now.Subtract(prevCapture).Seconds > 4)
+        {
+            print(DateTime.Now.ToString());
+            prevCapture = DateTime.Now;
+            SaveScreenshot();
+        }
+	}
+
+    void SaveScreenshot()
+    {
+        Application.CaptureScreenshot("Screenshots/capture_" + DateTime.Now.Ticks + ".png");
+    }
+}
